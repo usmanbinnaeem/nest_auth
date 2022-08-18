3e97f2ad9cc0eaa29fdde56a02a71e2c75e72d10
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-08-18 10:25:20 +0500

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { AuthService } from './modules/auth/auth.service';
+import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
+import { LocalAuthGuard } from './modules/auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) { }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    console.log("authorized user req", req);
+    return req.user;
+  }
+}
